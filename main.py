4fad08d714949c578d274ecc3a9367cd86aca571
afkpuezo <andrewfcurry@gmail.com>
2021-02-19 16:30:04 -0500

    tweaked to not need kafka

--- main.py
+++ main.py
@@ -16,26 +16,27 @@ from range_printing import print_range
 
 def main():
     """
-    Triggers the kafka producer to send instructions, then uses a different thread to execute
-    each of the actions in the instructed program's action list.
+    Triggers the kafka producer to send instructions, then uses a different thread to 
+    execute each of the actions in the instructed program's action list.
     """
     # prepare the producer
     programs = [] 
     programs.append(random.randint(1, 3)) # may have other way of picking later?
-    prod_thread = Thread(target = produce, kwargs = {'programs' : programs})
+    #prod_thread = Thread(target = produce, kwargs = {'programs' : programs})
     # consume those messages
-    consumer = KafkaConsumer('esthread')
+    #consumer = KafkaConsumer('esthread')
     #print("DEBUG: consumer initialized")
-    consumer.pause()
-    prod_thread.start()
-    consumer.resume()
+    #consumer.pause()
+    #prod_thread.start()
+    #consumer.resume()
     action_lists = []
     #print("DEBUG: about to start consumer loop")
-    for message in consumer:
-        if message.value == b'END':
-            break
-        else:
-            action_lists.append(get_action_list(message.value))
+    #for message in consumer:
+    #    if message.value == b'END':
+    #        break
+    #    else:
+    #        action_lists.append(get_action_list(message.value))
+    action_lists.append(get_action_list("Program:" + str(programs[0])))
     # execute the instructions
     print("Received instructions to execute the following programs:")
     for action_list in action_lists:
